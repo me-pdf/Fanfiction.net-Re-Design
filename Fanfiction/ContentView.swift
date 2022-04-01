e5e5914c914a7cf8b88e38a0e76eda2d0fba21fb
Piera di fusco <94469162+me-pdf@users.noreply.github.com>
2022-04-01 14:42:44 +0200

    Initial Commit

---
+++ Fanfiction/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fanfiction
+//
+//  Created by Piera Di Fusco on 01/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fanfiction/FanfictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanfictionApp.swift
+//  Fanfiction
+//
+//  Created by Piera Di Fusco on 01/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FanfictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
